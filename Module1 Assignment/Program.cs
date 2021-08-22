c081da63dbd0c604d4f0869c6a17310180ec5973
kouvang322 <kouvang322@hotmail.com>
2021-08-21 19:48:31 -0500

    Edited code

--- Module1 Assignment/Program.cs
+++ Module1 Assignment/Program.cs
@@ -6,7 +6,21 @@ namespace Module1_Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("How many Monsters did you drink today? ");
+            int monsterQty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You consumed:");
+
+            int calories = 220 * monsterQty;
+            int suger = 54 * monsterQty;
+            int caffine = 160 * monsterQty;
+
+            Console.WriteLine(calories + " Calories");
+            Console.WriteLine(suger + " Suger");
+            Console.WriteLine(caffine + "mg Caffine");
+            Console.WriteLine("Press any key to continue....");
+           
+        
         }
     }
 }
