211eea891d4eb9327e759c3c273b553fb2d7aa1c
kouvang322 <kouvang322@hotmail.com>
2021-08-21 23:50:30 -0500

    Module 1 Lab

---
+++ Module1 Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module1_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
