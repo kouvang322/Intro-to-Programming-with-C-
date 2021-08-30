21279430950e9f8d0e798d935f09001422d4319f
kouvang322 <kouvang322@hotmail.com>
2021-08-29 21:06:00 -0500

    Completed assignment 2
    confusing options combinations

--- Module2 Assignment/Program.cs
+++ Module2 Assignment/Program.cs
@@ -7,13 +7,92 @@ namespace Module2_Assignment
         static void Main(string[] args)
         {
             Console.Write("Is anyone attending the party a vegetarian? (y/n) ");
-            Console.ReadLine();
+            string isVeggie = (Console.ReadLine());
 
-            Console.Write("Is anyone attending the party a vegan? (y/n) ");
-            Console.ReadLine();
-            
-            Console.Write("Is anyone attending the party a gluten free? (y/n) ");
-            Console.ReadLine();
+            //  Confirming if correct information was entered for first question
+
+            if (isVeggie == "y" || isVeggie == "n")
+            {
+                Console.Write("Is anyone attending the party a vegan? (y/n) ");
+                string isVegan = (Console.ReadLine());
+                
+                // Confirming correct information for second question
+
+                if (isVegan == "y" || isVegan == "n")
+                {
+                    Console.Write("Is anyone attending the party gluten free? (y/n) ");
+                    string isGlutenFree = (Console.ReadLine());
+
+                    // Confirming correct information for final question
+
+                    if (isGlutenFree == "y" || isGlutenFree == "n")
+                    {
+
+                        // Options given depending on answers 
+
+                        if  ((isVeggie == "y" && isVegan == "y" && isGlutenFree == "y")||
+                            (isVeggie == "y" && isVegan == "y" && isGlutenFree == "n")||
+                            (isVeggie == "n" && isVegan == "y" && isGlutenFree == "y")||
+                            (isVeggie == "n" && isVegan == "y" && isGlutenFree == "n"))
+                        {
+                            //  Restricted to these two
+
+                            Console.WriteLine("You may order from the following restaurants:");
+                            Console.WriteLine("Cafe Manna");
+                            Console.WriteLine("Harvest Cafe");
+                        }
+                        else if ((isVeggie == "y" && isVegan == "n" && isGlutenFree == "y")||
+                                (isVeggie == "n" && isVegan == "n" && isGlutenFree == "y"))
+                        {
+                            //  Can still order even though don't need to accommodate vegans
+
+                            Console.WriteLine("You may order from the following restaurants:");
+                            Console.WriteLine("Marco's Pizza");
+                            Console.WriteLine("Cafe Manna");
+                            Console.WriteLine("Harvest Cafe");
+                        }
+                        else if (isVeggie == "y" && isVegan == "n" && isGlutenFree == "n")
+                        {
+                            //  Four places can still accommodate vegetarians
+
+                            Console.WriteLine("You may order from the following restaurants:");
+                            Console.WriteLine("Marco's Pizza");
+                            Console.WriteLine("Cafe Manna");
+                            Console.WriteLine("India Kitchen");
+                            Console.WriteLine("Harvest Cafe");
+                        }
+                        else if (isVeggie == "n" && isVegan == "n" && isGlutenFree == "n")
+                        {
+                            // No restrictions for attendees
+
+                            Console.WriteLine("You may order from the following restaurants:");
+                            Console.WriteLine("Thunder Bay Grill");
+                            Console.WriteLine("Marco's Pizza");
+                            Console.WriteLine("Cafe Manna");
+                            Console.WriteLine("India Kitchen");
+                            Console.WriteLine("Harvest Cafe");
+                        }
+                        else
+                        {
+                            // Results from other combinations besides options given, just in case
+
+                            Console.WriteLine("Sorry, there are no restaurants to accommodate attendees");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter y or n");
+                    }
+                }
+                 else
+                 {
+                    Console.WriteLine("Please enter y or n");
+                 }
+            }
+            else
+            {
+                Console.WriteLine("Please enter y or n");
+            }
         }
     }
 }
