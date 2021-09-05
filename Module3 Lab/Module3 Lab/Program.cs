66fa20a975d31e4a8405d28706e65a703616e847
kouvang322 <kouvang322@hotmail.com>
2021-09-04 19:36:56 -0500

    Switching from VSCode to VS 2019 due to Omnisharp issues

---
+++ Module3 Lab/Module3 Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Test test test");
+
+
+            Console.Read();
+
+        }
+    }
+}
